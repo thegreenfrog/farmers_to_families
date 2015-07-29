25aeee6d5111f48bcb8a0c5abbf14c54e213c25e
Chris Lu <gogreen.chris@gmail.com>
2015-07-29 14:48:24 -0400

    Added checks when signing up

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,13 @@
 Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
+    requestPermissions: {
+      facebook: ['email']
+    },
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
+
+Accounts.validateNewUser(function(user) {
+   if(user.username && user.username.length > 3) {
+        return true;
+   }
+    throw new Meteor.Error(403, "Username has to be longer than 3 characters");
 });
