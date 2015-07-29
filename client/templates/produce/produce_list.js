1f72954a649d9ec0800268a48db25e56e049d2cc
Chris Lu <gogreen.chris@gmail.com>
2015-07-28 23:24:17 -0400

    fixed ordering of lists. Finally understand Publications and Subscriptions, changed naming/location of stuff related to that.

--- client/templates/produce/produce_list.js
+++ client/templates/produce/produce_list.js
@@ -1,5 +1,5 @@
 Template.produceList.helpers({
-   posts: function() {
-       return Posts.find({}, {sort: {submitted: -1}});
-   }
+    posts: function() {
+        return Posts.find({}, {sort: {joined: -1}});
+    }
 });
--- server/publications.js
+++ server/publications.js
@@ -1,16 +1,3 @@
-Meteor.publish('posts', function(options) {
-    check(options, Object);
 
-    return Posts.find({}, options);
-});
 
-Meteor.publish('farmers', function(options) {
-    check(options, Object);
 
-    return Farmers.find({}, options);
-});
-
-Meteor.publish('singleEdit', function(userId) {
-    check(userId, String);
-    return Posts.find(userId);
-});
