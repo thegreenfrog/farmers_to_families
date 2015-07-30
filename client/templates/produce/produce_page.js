f32020f6095360e82c08f19a903fb46d03690a38
Chris Lu <gogreen.chris@gmail.com>
2015-07-30 10:35:15 -0400

    Added time element to post_page.

--- client/templates/produce/produce_page.js
+++ client/templates/produce/produce_page.js
@@ -1,5 +1,32 @@
 Template.producePage.helpers({
     ownPost: function() {
-        return this.userId === Meteor.userId();
+        return this.item.userId === Meteor.userId();
+    },
+    time: function() {
+        //get the date mm/dd/yyyy
+        var today = this.item.joined;
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        var hour = today.getHours();
+        var min = today.getMinutes();
+        if(dd<10) {
+            dd='0'+dd
+        }
+        if(mm<10) {
+            mm='0'+mm
+        }
+        var halfday = 'AM';
+        if(hour === 24) {
+            hour -=12;
+        } else if(hour > 12) {
+            hour -= 12
+            halfday = 'PM';
+        }
+        if(min<10) {
+            min = '0'+min;
+        }
+        today = mm+'/'+dd+'/'+yyyy + '  ' +hour+':'+min+' '+halfday;
+        return today;
     }
 });
