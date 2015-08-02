5d4ba33db7de7d27e4c5cd1069705ab03f12f854
Chris Lu <gogreen.chris@gmail.com>
2015-08-02 10:59:58 -0400

    Implemented error handling in edit submit page

--- client/templates/produce/produce_edit.js
+++ client/templates/produce/produce_edit.js
@@ -1,6 +1,13 @@
-
+Template.produceEdit.onCreated(function() {
+    Session.set('produceEditErrors', {});
+});
 Template.produceEdit.helpers({
-
+    errorClass: function(field) {
+        return !!Session.get('produceEditErrors')[field] ? 'has-error' : '';
+    },
+    errorMessage: function(field) {
+        return Session.get('produceEditErrors')[field];
+    }
 });
 
 Template.produceEdit.events({
@@ -13,6 +20,13 @@ Template.produceEdit.events({
             produce: $(e.target).find('[name=produce]').val(),
             price: parseFloat($(e.target).find('[name=price]').val())
         };
+
+        var errors = validatePost(postProperties);
+        if(errors.produce || errors.price) {
+            console.log('errors');
+            return Session.set('produceEditErrors', errors);
+        }
+
         Posts.update(postId, {$set: postProperties}, function(error) {
             if (error) {
                 // display the error to the user
