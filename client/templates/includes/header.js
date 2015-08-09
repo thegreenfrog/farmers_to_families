f837ae7eb2b61a5bfe1eedf9b5f1a538796c1906
Chris Lu <gogreen.chris@gmail.com>
2015-08-09 19:21:13 -0400

    Completed logic for searching database for submitted search query.

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -12,5 +12,16 @@ Template.header.events({
     'keypress #search-button': function() {
         $('button').prop('disabled', false);
         console.log('disabled button');
+    },
+    'submit form': function(e) {
+        e.preventDefault();
+        console.log('searching for something');
+        //break up the text into individual words, pass them as array
+        var searchSubmit = $(e.target).find('[name=text]').val();
+        console.log(searchSubmit);
+        var keys = String(searchSubmit).split(" ");
+        Meteor.call('searchDatabase', keys, function(error, result) {
+
+        });
     }
 });
