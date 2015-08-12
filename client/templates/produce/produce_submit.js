3450c91ffc3ee31fb662b77898f662760a9ae389
Chris Lu <gogreen.chris@gmail.com>
2015-08-12 17:05:46 -0400

    Fixed general layout of produce submit page to categorize posts better. Dropdown lists added to allow user to categorize produce. Produce field is now just a specific title farmers give produce.

--- client/templates/produce/produce_submit.js
+++ client/templates/produce/produce_submit.js
@@ -1,4 +1,11 @@
 Template.produceSubmit.events({
+    'click .category': function(e) {
+        e.preventDefault();
+        console.log($(e.target).text());
+        var newTitle = $(e.target).text();
+        Session.set('category-title', newTitle);
+        console.log('changing dropdown title');
+    },
    'submit form': function(e){
        e.preventDefault();
 
@@ -26,13 +33,25 @@ Template.produceSubmit.events({
 
 Template.produceSubmit.onCreated(function() {
    Session.set('produceSubmitErrors', {});
+    Session.set('category-title', 'Select a Category');
 });
 
 Template.produceSubmit.helpers({
+    categoryTitle: function() {
+        return Session.get('category-title');
+    },
    errorClass: function(field) {
        return !!Session.get('produceSubmitErrors')[field] ? 'has-error' : '';
    },
     errorMessage: function(field) {
         return Session.get('produceSubmitErrors')[field];
     }
+});
+
+$(".category").click(function() {
+    console.log('clicked');
+});
+
+$("#category-title").click(function() {
+    console.log('clicked');
 });
