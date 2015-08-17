e9e16b2ba48e8b070d9d248c7fba3d93be464c0a
Chris Lu <gogreen.chris@gmail.com>
2015-08-16 22:51:38 -0400

    Sub filter for each category appears after user clicks on category to filter.

--- client/templates/misc/sub_filter_list.js
+++ client/templates/misc/sub_filter_list.js
@@ -1,11 +1,6 @@
 Template.subFilter.helpers({
-    subCategory: function() {
-        if(String('vegetable') == String(this)) {
-            return ['Carrots', 'Squash', 'Tomatoes', 'Potatoes', 'Kale', 'Avocado'];
-        } else if(String('fruit') == String(this)){
-            return ['Apples', 'Oranges', 'Peaches', 'Pear', 'Grapes', 'Strawberries'];
-        } else {
-            return ['Milk', 'Cheese', 'Yogurt'];
-        }
+    currentCategory: function(category) {
+            check(category, String);
+            return !(String(category) == String(this));
     }
 });
--- client/templates/produce/produce_filter_list.js
+++ client/templates/produce/produce_filter_list.js
@@ -2,8 +2,51 @@ Template.produceListFilter.helpers({
     posts: function() {
         return Posts.find({}, {sort: {joined: -1}});
     },
-    currentCategory: function(category) {
-        check(category, String);
-        return !(String(category) == String(this));
+    currentCategory: function() {
+        //console.log(this);
+        if(String(this) == Session.get('category')){
+            console.log('matching category');
+            return true;
+        }
+        return false;
+    },
+    currentSubCategory: function() {
+        if(String(this) == Session.get('subcategory')){
+            console.log('matching subcat');
+            return true;
+        }
+        return false;
+    },
+    category: function() {
+        return ['Vegetable', 'Fruit', 'Dairy'];
+    },
+    subCategory: function() {
+        console.log('compare');
+        console.log(String(this));
+        console.log(Session.get('category'));
+        if(Session.get('category') == 'Vegetable') {
+            return ['Carrots', 'Squash', 'Tomatoes', 'Potatoes', 'Kale', 'Avocado'];
+        } else if(Session.get('category') == "Fruit"){
+            return ['Apples', 'Oranges', 'Peaches', 'Pear', 'Grapes', 'Strawberries'];
+        } else {
+            return ['Milk', 'Cheese', 'Yogurt'];
+        }
+    }
+});
+
+Template.produceListFilter.events({
+    'click .filter-link': function(e) {
+        var $parent = $(e.target).closest('div');
+        //console.log($parent.attr('id'));
+        if($parent.attr('id') == "category-filter") {
+            //console.log($(e.target).attr('id'));
+            Session.set('category', $(e.target).attr('id'));
+            console.log('set category')
+            Session.set('subCategory', '');
+            //console.log('category');
+        } else {
+            Session.set('subcategory', $(e.target).attr('id'));
+            //console.log('subcat');
+        }
     }
 });
